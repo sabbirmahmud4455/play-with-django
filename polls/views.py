edeb64bd6f4745f1eca2fc877519988505e48b04
sabbirmahmud4455 <sabbirmahmud4455@gmail.com>
2025-04-07 22:42:49 +0600

    create and data show in polls details view

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
-
+from django.http import Http404
 # Create your views here.
 def index(request):
-	latest_question_list =  Question.objects.order_by("-pub_date")[:5]
-	latest_question_list = ", ".join([str(q) for q in latest_question_list])
+	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+	latest_question_list = list(latest_question_list.values())
 
 	context = {
 		"latest_question_list": latest_question_list,
@@ -13,10 +13,9 @@ def index(request):
 
 	return render(request, "polls/index.html", context)
 
-	return HttpResponse(latest_question_list)
-
 def detail(request, question_id):
-	return HttpResponse(f"You're looking at question {question_id}.")
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
 	return HttpResponse(f"You're looking at the results of question {question_id}.")
